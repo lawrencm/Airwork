741626ef260a63cb3ad5041ca1deefd3536dbd92
Matt <lawrencm@gmail.com>
2013-07-30 17:37:08 +0800

    Added client side association pre-population

---
+++ set_env.sh
@@ -0,0 +1,7 @@
+export MAVEN_OPTS="-Xms1024m -Xmx2048m -XX:MaxPermSize=256m"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home"
+export PATH=$PATH:"/Applications/alfresco-4.2.c/libreoffice.app/Contents/MacOS"
+
+echo "Exported path: $PATH"
+echo "Exported Java Home: $JAVA_HOME"
+echo "Exported Maven Opts: $MAVEN_OPTS"
--- share/src/main/amp/web/scripts/components/documentlibrary/combine-docs-action.js
+++ share/src/main/amp/web/scripts/components/documentlibrary/combine-docs-action.js
@@ -1,4 +1,3 @@
-
 /**
  * Document Browse and Details page email actions
  *
@@ -11,92 +10,115 @@
      * @method onActionSendEmail
      * @param file the file to be sent with email (Object Literal)
      */
-    YAHOO.Bubbling.fire("registerAction",
- {
-        actionName: "onActionCombineAllDocs",
-        fn: function mycompany_onActionSendAsEmail(file) {
+    // YAHOO.Bubbling.fire("registerAction", {
+    //     actionName: "onActionCombineAllDocs",
+    //     fn: function mycompany_onActionSendAsEmail(file) {
 
-            console.log("testme2");
-            console.log(file);
+    //         console.log("testme2");
+    //         console.log(file);
 
-            var templateUrl = YAHOO.lang.substitute(Alfresco.constants.URL_SERVICECONTEXT + "components/form?itemKind={itemKind}&itemId={itemId}&destination={destination}&mode={mode}&submitType={submitType}&showCancelButton=true&formId={formId}", {
-                itemKind: "type",
-                itemId: "md:compoundDocScaffold",
-                mode: "edit",
-                destination:file.nodeRef,
-                submitType: "json",
-                formId: "mergedocxformall" 
-            });
+    //         var templateUrl = YAHOO.lang.substitute(Alfresco.constants.URL_SERVICECONTEXT + "components/form?itemKind={itemKind}&itemId={itemId}&destination={destination}&mode={mode}&submitType={submitType}&showCancelButton=true&formId={formId}", {
+    //             itemKind: "type",
+    //             itemId: "md:compoundDocScaffold",
+    //             mode: "edit",
+    //             destination: file.nodeRef,
+    //             submitType: "json",
+    //             formId: "mergedocxformall"
+    //         });
 
-            var actionUrl = YAHOO.lang.substitute("{stem}slingshot/doclib/action/mergedocx?nodeRef={nodeRef}&AllDocs=1",{
-                stem:Alfresco.constants.PROXY_URI,
-                nodeRef:file.nodeRef
-            });
-
+    //         var actionUrl = YAHOO.lang.substitute("{stem}slingshot/doclib/action/mergedocx?nodeRef={nodeRef}&AllDocs=1", {
+    //             stem: Alfresco.constants.PROXY_URI,
+    //             nodeRef: file.nodeRef
+    //         });
 
 
 
-            var editDetails = new Alfresco.module.SimpleDialog(this.id + "-editDetails-" + Alfresco.util.generateDomId());
+    //         var editDetails = new Alfresco.module.SimpleDialog(this.id + "-editDetails-" + Alfresco.util.generateDomId());
 
-            editDetails.setOptions({
-                width: "40em",
-                templateUrl: templateUrl,
-                actionUrl: actionUrl,
-                destroyOnHide: true,
-                method: Alfresco.util.Ajax.POST,
-                onSuccess: {
-                    fn: function dlA_onActionDetails_success(response) {
-                        console.log(response);
-                        Alfresco.util.PopupManager.displayMessage({
-                            text: this.msg("message.details.success")
-                        });
+    //         editDetails.setOptions({
+    //             width: "40em",
+    //             templateUrl: templateUrl,
+    //             actionUrl: actionUrl,
+    //             destroyOnHide: true,
+    //             doBeforeDialogShow: {
+    //                 fn: function xxx(p_form, p_dialog) {
+    //                     //console.log(p_form,p_dialog);
+    //                     var fileSpan = '<span class="light">Combine all files (*.docx)</span>';
 
-                    },
-                    scope: this
-                },
-                onFailure: {
-                    fn: function dLA_onActionDetails_failure(response) {
-                        Alfresco.util.PopupManager.displayMessage({
-                            text: this.msg("message.details.failure")
-                        });
-                    },
-                    scope: this
-                }
-            });
+    //                     // console.log(p_form);
+    //                     // Alfresco.util.populateHTML(["template_x002e_folder-actions_x002e_folder-details_x0023_default-editDetails-alf-id4-form-container_h","xxxxx"]);
+    //                     console.log(p_dialog.id + "-form-container_h");
 
-            // console.log("got here");
-            editDetails.show();
+    //                     // "template_x002e_folder-actions_x002e_folder-details_x0023_default-editDetails-alf-id4"
+    //                     // "template_x002e_folder-actions_x002e_folder-details_x0023_default-editDetails-alf-id4-form-container_h
+    //                     Alfresco.util.populateHTML([p_dialog.id + "-form-container_h", fileSpan]);
+    //                 },
+    //                 scope: this
+    //             },
 
-        }
-    });
+    //             method: Alfresco.util.Ajax.POST,
+    //             onSuccess: {
+    //                 fn: function dlA_onActionDetails_success(response) {
+    //                     console.log(response);
+    //                     Alfresco.util.PopupManager.displayMessage({
+    //                         text: this.msg("message.details.success")
+    //                     });
+
+    //                 },
+    //                 scope: this
+    //             },
+    //             onFailure: {
+    //                 fn: function dLA_onActionDetails_failure(response) {
+    //                     Alfresco.util.PopupManager.displayMessage({
+    //                         text: this.msg("message.details.failure")
+    //                     });
+    //                 },
+    //                 scope: this
+    //             }
+    //         });
+
+    //         // console.log("got here");
+    //         editDetails.show();
+
+    //     }
+    // });
 
 
 
-    YAHOO.Bubbling.fire("registerAction",
-    {
+    YAHOO.Bubbling.fire("registerAction", {
         actionName: "onActionCombineSelectedDocs",
         fn: function mycompany_onActionSendAsEmail(file) {
 
-            console.log("testme2");
-            console.log(file);
+            // console.log("testme2");
+            // console.log(file);
 
             var templateUrl = YAHOO.lang.substitute(Alfresco.constants.URL_SERVICECONTEXT + "components/form?itemKind={itemKind}&itemId={itemId}&destination={destination}&mode={mode}&submitType={submitType}&showCancelButton=true", {
                 itemKind: "type",
                 itemId: "md:compoundDocScaffold",
                 mode: "edit",
-                destination:file.nodeRef,
+                destination: file.nodeRef,
                 submitType: "json"
 
                 // formId: "mergedocxform" 
             });
 
-            var actionUrl = YAHOO.lang.substitute("{stem}slingshot/doclib/action/mergedocx?nodeRef={nodeRef}",{
-                stem:Alfresco.constants.PROXY_URI,
-                nodeRef:file.nodeRef
+            var actionUrl = YAHOO.lang.substitute("{stem}slingshot/doclib/action/mergedocx?nodeRef={nodeRef}", {
+                stem: Alfresco.constants.PROXY_URI,
+                nodeRef: file.nodeRef
             });
 
 
-            console.log("testme3");
+
+            var docsUrls = YAHOO.lang.substitute("{stem}slingshot/doclib/doclist/cm:content/node/{nodeUri}", {
+                stem: Alfresco.constants.PROXY_URI,
+                nodeUri: file.jsNode.nodeRef.uri
+            });
+
+            //console.log(file.jsNode.nodeRef.uri);
+
+
+
+            //console.log("testme3");
 
             var editDetails = new Alfresco.module.SimpleDialog(this.id + "-editDetails-" + Alfresco.util.generateDomId());
 
@@ -105,11 +127,66 @@
                 templateUrl: templateUrl,
                 actionUrl: actionUrl,
                 destroyOnHide: true,
+                title: "Select the Docx files to combine",
                 method: Alfresco.util.Ajax.POST,
+                doBeforeDialogShow: {
+                    fn: function xxx(p_form, p_dialog) {
+                        //console.log(p_form,p_dialog);
+                        var fileSpan = '<span class="light">Combine all files (*.docx)</span>';
+
+                        // console.log(p_form);
+                        // Alfresco.util.populateHTML(["template_x002e_folder-actions_x002e_folder-details_x0023_default-editDetails-alf-id4-form-container_h","xxxxx"]);
+                        // console.log(p_dialog.id + "-form-container_h");
+
+                        // console.log(docsUrls);
+                        var widget = Alfresco.util.ComponentManager.get(p_dialog.id + "_assoc_md_generateFrom-cntrl");
+                        Alfresco.util.Ajax.jsonGet({
+                            url: docsUrls,
+                            successCallback: {
+                                fn: function ajax_success(response) {
+                                    console.log(response);
+
+                                    var items = response.json.items;
+                                    var nodes = [];
+
+                                    for (var i = 0; i < items.length; i++) {
+                                        if (items[i].mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+                                            nodes.push(items[i].nodeRef);
+
+                                        }
+
+                                    };
+                                    widget.selectItems(nodes.join(","));
+
+                                },
+                                scope: this
+                            },
+                            failureCallback: {
+                                fn: function ajax_failure(response) {
+                                    console.log(response);
+                                },
+                                scope: this
+                            }
+                        });
+
+
+                        // "template_x002e_folder-actions_x002e_folder-details_x0023_default-editDetails-alf-id4"
+                        // "template_x002e_folder-actions_x002e_folder-details_x0023_default-editDetails-alf-id4-form-container_h
+                        Alfresco.util.populateHTML([p_dialog.id + "-form-container_h", fileSpan]);
+
+                        // template_x002e_folder-actions_x002e_folder-details_x0023_default-editDetails-alf-id6_assoc_md_generateFrom-cntrl
+
+
+
+                        // _assoc_md_generateFrom-cntrl
+
+                    },
+                    scope: this
+                },
                 onSuccess: {
                     fn: function dlA_onActionDetails_success(response) {
-                        console.log(response);
-                        console.log("worked");
+                        // console.log(response);
+                        // console.log("worked");
                         Alfresco.util.PopupManager.displayMessage({
                             text: this.msg("message.details.success")
                         });
@@ -119,7 +196,7 @@
                 },
                 onFailure: {
                     fn: function dLA_onActionDetails_failure(response) {
-                        console.log("failed");
+                        // console.log("failed");
                         Alfresco.util.PopupManager.displayMessage({
                             text: this.msg("message.details.failure")
                         });
@@ -128,7 +205,7 @@
                 }
             });
 
-            console.log("test final");
+            // console.log("test final");
             editDetails.show();
 
         }
